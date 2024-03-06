d64c1d1ebb22e202dc710327250cf9a3891492d2
Bohdan Davydenko <yukon197415@gmail.com>
2024-03-06 21:25:20 +0200

    Add optional user's request verifying

--- src/dependencies/user_dependencies.py
+++ src/dependencies/user_dependencies.py
@@ -3,7 +3,7 @@ import json
 from jwt import decode
 from fastapi import Request, HTTPException, status
 
-from src.config.settings import USER_MICROSERVICE_KEY, USER_MICROSERVICE_BASE_URL, JWT_SIGNING_KEY
+from src.config.settings import USER_MICROSERVICE_KEY, USER_MICROSERVICE_BASE_URL, JWT_SIGNING_KEY, VERIFY_USERS_REQUESTS
 
 def is_staff_user(request: Request):
     """
@@ -21,11 +21,14 @@ def is_staff_user(request: Request):
 
     user_id = decode(token,JWT_SIGNING_KEY ,algorithms=["HS256"]).get("user_id")
 
-    user_data = requests.post(f"{USER_MICROSERVICE_BASE_URL}/api/user/check/",
-                              {"user_id": int(user_id), "microservice_key": USER_MICROSERVICE_KEY})
-    status_code = user_data.status_code
-    user_data = json.loads(user_data.content)
-    if (user_data.get("is_superuser", False) or user_data.get("is_staff", False)) and status_code == 200:
+    if VERIFY_USERS_REQUESTS:
+        user_data = requests.post(f"{USER_MICROSERVICE_BASE_URL}/api/user/check/",
+                                  {"user_id": int(user_id), "microservice_key": USER_MICROSERVICE_KEY})
+        status_code = user_data.status_code
+        user_data = json.loads(user_data.content)
+        if (user_data.get("is_superuser", False) or user_data.get("is_staff", False)) and status_code == 200:
+            return True
+    else:
         return True
 
     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not allowed to perform this action")
